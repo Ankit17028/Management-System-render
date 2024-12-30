3e662321a4b99b5fe1a92adc9093584f114aa48a
Ankit17028 <ankitbansod2000@gmail.com>
2024-12-31 01:09:07 +0530

    Generate .env file

--- src/main/java/com/Ankit/KAM/KamApplication.java
+++ src/main/java/com/Ankit/KAM/KamApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class KamApplication {
+public class
+
+KamApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KamApplication.class, args);
